9ea998a0bbffc868996f896eef18122cb828e1ec
adorjns <adorjns@gmail.com>
2020-04-05 16:41:13 +0200

    add basic routes

--- SPA/hazard-mapper/src/App.tsx
+++ SPA/hazard-mapper/src/App.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Register from "./components/register";
+import {Login} from "./components/login";
+import MainMap from "./components/mainMap";
 
 function App() {
   return (
       <div className="App">
-        <header className="App-header">
-        </header>
+          <Router>
+              <div>
+                  <nav>
+                      <ul>
+                          <li>
+                              <Link to="/">Home</Link>
+                          </li>
+                          <li>
+                              <Link to="/register">Register</Link>
+                          </li>
+                          <li>
+                              <Link to="/login">Login</Link>
+                          </li>
+                      </ul>
+                  </nav>
+
+                  <Switch>
+                      <Route path="/register">
+                          <Register />
+                      </Route>
+                      <Route path="/login">
+                          <Login />
+                      </Route>
+                      <Route path="/">
+                          <MainMap />
+                      </Route>
+                  </Switch>
+              </div>
+          </Router>
       </div>
   );
 }
---
+++ SPA/hazard-mapper/src/components/mainMap.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class MainMap extends React.Component {
+    render() {
+        return <h1>Main map</h1>;
+    }
+}
+
+export default MainMap;
